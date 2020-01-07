e899dec192c6ac275cc6eddfeae8a2089c98d6c7
Kenneth Lee <kennethlee@fortinet.com>
2020-01-07 20:27:25 +0000

    fix(pack): clean up

--- package.sh
+++ package.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 rm -f SHA256SUMS
-sha256sum manifest.json package.json *.js LICENSE README.md > SHA256SUMS
-find css js views -type f -exec sha256sum {} \; >> SHA256SUMS
+sha256sum manifest.json package.json *.js **/*.js README.md > SHA256SUMS
 npm pack
