afb375daaef770555b488739d235e943928d0c7b
Kenneth Lee <kennethlee@fortinet.com>
2020-01-09 17:47:26 +0000

    chore(editor): newline at eof

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const manifest = require('./manifest.json')
 const {MyQAdapter} = require('./myq')
 const {Database} = require('gateway-addon')
 
-
 module.exports = async (addonManager) => {
   let db = new Database(manifest.id)
   let config = await db.open().then(() => db.loadConfig())
