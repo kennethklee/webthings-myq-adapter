1d335c650fd842139718ee5d4ec6b0dc06cd5d18
Kenneth Lee <kennethlee@fortinet.com>
2020-01-09 17:47:03 +0000

    feat(package): backup node_modules incase of dev deps

--- package.sh
+++ package.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-rm -rf node_modules
+mv node_modules node_modules.bak  # backup node_modules
+
+echo 'Generating package...'
 npm install --production
 rm -f SHA256SUMS
 sha256sum manifest.json package.json *.js LICENSE **/*.js README.md > SHA256SUMS
@@ -8,8 +10,9 @@ rm -rf node_modules/.bin
 find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
 TARFILE=$(npm pack)
 tar xzf ${TARFILE}
-cp -r node_modules ./package
+mv node_modules package
 tar czf ${TARFILE} package
-# rm -rf package
-sha256sum ${TARFILE}
-echo "Created ${TARFILE}"
+rm -rf package
+mv node_modules.bak node_modules  # restore node_modules
+sha256sum --tag ${TARFILE}
+echo "Created package, ${TARFILE}"
